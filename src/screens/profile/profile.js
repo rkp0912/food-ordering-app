b44f37feddee198c5e34b1507af971a891e4edb2
E532650 <Krishna.Praveen@Honeywell.com>
2020-06-06 18:29:22 +0530

    Bugfixes - retaining login and register text, redirecting to empty profile page, maintain firstname in session storage

---
+++ src/screens/profile/profile.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Header from '../../common/Header';
+
+
+class Profile extends Component {
+    render(){ 
+        return(
+            <div>
+                <Header/>
+            </div>
+        )
+    }
+}
+export default Profile;
